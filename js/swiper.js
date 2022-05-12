6079a91154b038d53327151defd19aa3da4ab511
Maxim <maxRMD@yandex.ru>
2022-05-12 14:17:44 +0300

    osnova

---
+++ js/swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+    // navigation: {
+    // nextEl: '.swiper-button-next',
+    // prevEl: '.swiper-button-prev',
+    // },
+
+    // // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+    spaceBetween: 16,
+    // autoHeight: true,
+});
